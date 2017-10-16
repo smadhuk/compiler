1e7b63d9c0b8d4c195f92b50bc98c8adce50c4a5
Siddhant Madhuk <siddhant@quantifind.com>
2017-10-15 20:05:22 -0700

    reorganized file strucutre and set up unit testing

--- slinterp.ml
+++ slinterp.ml
@@ -1,28 +1,4 @@
-open Slp.SLI
-
-(* type id = string;;
- * 
- * type binop = Plus | Minus | Times | Div;;
- * 
- * type   stm = CompoundStm of stm * stm 
- *            | AssignStm of id * exp
- *            | PrintStm of exp list
- * 
- *    and exp = IdExp of id
- *            | NumExp of int
- *            | OpExp of exp * binop * exp
- *            | EseqExp of stm * exp;;
- * 
- *
- *)
- 
-(* a := 5 + 3; b := (print(a,a-1), 10*a); print (b) *)  
-let prog = CompoundStm(AssignStm("a", OpExp(NumExp 5, Plus, NumExp 3)), 
-             CompoundStm(AssignStm("b",
-               EseqExp(PrintStm[IdExp "a"; OpExp(IdExp "a", Minus,
-                                                 NumExp 1)],
-                  OpExp(NumExp 10, Times, IdExp "a"))),
-                    PrintStm[IdExp "b"]));;
+open Slp 
 
 
 let rec maxargs s =
@@ -41,4 +17,4 @@ let rec maxargs s =
                  | _             -> 0)
 ;;
 
-let test = maxargs prog;;
+
--- slp.ml
+++ slp.ml
@@ -1,19 +1,12 @@
-module SLI = struct
+type id = string;;
 
-  type id = string;;
-  
-  type binop = Plus | Minus | Times | Div;;
-  
-  type   stm = CompoundStm of stm * stm 
-             | AssignStm of id * exp
-             | PrintStm of exp list
-  
-     and exp = IdExp of id
-             | NumExp of int
-             | OpExp of exp * binop * exp
-             | EseqExp of stm * exp;;
+type binop = Plus | Minus | Times | Div;;
 
-end  
+type   stm = CompoundStm of stm * stm 
+           | AssignStm of id * exp
+           | PrintStm of exp list
 
-
-
+   and exp = IdExp of id
+           | NumExp of int
+           | OpExp of exp * binop * exp
+           | EseqExp of stm * exp;;
